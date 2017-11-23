61d11bc43a548b4f578dc3acfdbad78619965c08
Andy Rixon <rixon.andy@gmail.com>
2017-11-23 20:34:50 +0000

    Updates

--- install/config.php
+++ install/config.php
@@ -19,7 +19,7 @@ define('MYSQL_PORT', 3306);
  * Iko Local Details
  */
 define('SITE_URL', '%site_url%');//Without the ending "/"
-define('LayerBB_VERSION', '1.0.0 RC1');
+define('LayerBB_VERSION', '1.0.0 BETA 2');
 define('LAYER_SESSION_TIMEOUT', 31536000);//In seconds.
 define('USER_PASSWORD_HASH_COST', 10);
 
